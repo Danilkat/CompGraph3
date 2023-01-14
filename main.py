95a2fd4c6d4bcb5e09a09abc30e927754bf84200
Danil Katashev <katashnya@gmail.com>
2023-01-15 01:33:33 +0700

    fractal now fit in window

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from svg_turtle import SvgTurtle
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPM
+import sys
+import math
 
 class LSystem:
-
 
     def __init__(self, iterations:int, segment_len:int):
         self.iterations = iterations
         self.segment_len = segment_len
         self.angle = 20.0
-        self.rules = []
         self.turtle = SvgTurtle()
         self.saved_position = []
         self.saved_angle = []
 
-        self.axiom = []
+        self.axiom = ""
         self.rules = {}
 
-        self.compiled = []
+        self.compiled = ""
 
         self.commands = {
             '+': self.terminalRotateCW,
@@ -46,71 +46,119 @@ class LSystem:
         self.turtle.pendown()
 
     def parse_str(self, input_lines):
-        self.axiom = ProductionRule(input_lines.pop(0), self.commands).commands
+        self.axiom = ProductionRule(input_lines.pop(0)).commands
         self.angle = float(input_lines.pop())
         self.rules = {}
         for line in input_lines:
-            rule_buff = ProductionRule(line, self.commands)
+            rule_buff = ProductionRule(line)
             self.rules[rule_buff.letter] = rule_buff.commands
         return self
 
     def compile(self):
         self.compiled = self.axiom
         for i in range(self.iterations):
+            print(f"compiling iteration {i} of {self.iterations}")
             self.compiled = self.compile_iteration(self.compiled)
+            print(f"finished compiling iteration {i} of {self.iterations}")
+        print("finished compiling {self.iterations} iterations")
         return self
 
     def compile_iteration(self, input):
-        output = []
+        output = ""
         for letter in input:
             if letter in self.rules:
                 output += self.rules[letter]
             else:
-                output.append(letter)
+                output += letter
         return output
 
-    def run(self):
+    def prerun(self):
+        x_ll_corner = 0
+        y_ll_corner = 0
+        x_ur_corner = 0
+        y_ur_corner = 0
+
         for command in self.compiled:
-            if command in self.commands:
+            if command in self.commands.keys():
                 self.commands[command]()
             else:
                 self.terminalForward()
 
+            x_ll_corner, x_ur_corner, y_ll_corner, y_ur_corner = self.update_aabb(
+                x_ll_corner,
+                x_ur_corner,
+                y_ll_corner,
+                y_ur_corner
+                )
+        x_ll_corner = math.floor(x_ll_corner)
+        y_ll_corner = math.floor(y_ll_corner)
+        x_ur_corner = math.ceil(x_ur_corner)
+        y_ur_corner = math.ceil(y_ur_corner)
+
+        x_center = (x_ur_corner + x_ll_corner) / 2
+        y_center = (y_ur_corner + y_ll_corner) / 2
+
+        self.turtle = SvgTurtle(width = x_ur_corner - x_ll_corner, height = y_ur_corner - y_ll_corner)
+        self.turtle.penup()
+        self.turtle.setposition(-x_center, -y_center)
+        self.turtle.pendown()
+        print("finished prerun")
+
+    def run(self):
+        print("start running...")
+        self.prerun()
+        for command in self.compiled:
+            if command in self.commands.keys():
+                self.commands[command]()
+            else:
+                self.terminalForward()
+        
+
+    def update_aabb(self, x0, x1, y0, y1, x = None, y = None):
+        if x is None:
+            x = self.turtle.xcor()
+        if y is None:
+            y = self.turtle.ycor()
+        x0 = min(x0, x)
+        x1 = max(x1, x)
+        y0 = min(y0, y)
+        y1 = max(y1, y)
+        return x0, x1, y0, y1
+
 
 class ProductionRule:
 
-    def __init__(self, rule_str, parent_commands: dict):
+    def __init__(self, rule_str):
 
-        self.commands = []
+        self.commands = ""
         self.letter = ""
-        self.parse(rule_str, parent_commands)
+        self.parse(rule_str)
         pass
 
-    def parse(self, rule_str, parent_commands):
-        if self.commands != []:
+    def parse(self, rule_str):
+        if self.commands != "":
             return self.commands
         rule_str = rule_str.split(" ")
         self.letter = rule_str.pop(0)
         rule_str.pop(0)
-        for letter in rule_str[0]:
-            if letter in parent_commands:
-                self.commands.append(parent_commands[letter])
-            else:
-                self.commands.append(letter)
+        self.commands = rule_str[0]
         return self.commands
         
-def main():
-    file = open("data.txt", "r")
+def main(input_str, iterations):
+    file = open(input_str, "r")
     lines = file.readlines()
+    lines = [line.strip() for line in lines]
     file.close()
-    lsyst = LSystem(5, 1).parse_str(lines).compile()
+    lsyst = LSystem(int(iterations), 10).parse_str(lines).compile()
     lsyst.run()
-    name = "output.svg"
-    lsyst.turtle.save_as(name)
-    drawing = svg2rlg(name)
-    renderPM.drawToFile(drawing, "output.png", fmt="PNG")
 
+    name_svg = input_str.split(".")[0] + ".svg"
+    name_png = input_str.split(".")[0] + ".png"
+
+    lsyst.turtle.save_as(name_svg)
+    drawing = svg2rlg(name_svg)
+    renderPM.drawToFile(drawing, name_png, fmt="PNG")
 
 
 if __name__ == '__main__':
-  main()
+    main(*sys.argv[1:])
